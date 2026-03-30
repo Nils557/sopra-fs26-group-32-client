14054c0de50c666b47651ac56e3782a93706650f
Nils <nilsojustdoesit@gmail.com>
2026-03-30 09:29:45 +0200

    changed tab name to GeoGuess City #115

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Student XX-XXX-XXX",
+  title: "GeoGuess City",
   description: "sopra-fs26-template-client",
 };
 
