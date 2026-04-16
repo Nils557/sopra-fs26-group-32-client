4644094e2190fc375e51435e8d2e824f9bb2f98f
faiaz18 <faiaz.islam2004gmail.com>
2026-04-16 14:28:32 +0200

    revert to state at dc1dbf2

--- app/hooks/useWebSocket.ts
+++ app/hooks/useWebSocket.ts
@@ -8,17 +8,13 @@ export const useWebSocket = <T,>(topic: string, onMessage: (msg: T) => void) => 
   useEffect(() => {
     const userId = sessionStorage.getItem('userId')?.replace(/"/g, '');
     const wsUrl = getWsDomain();
-
     if (!userId || userId === 'undefined') return;
 
     const stompClient = new Client({
-      // eslint-disable-next-line @typescript-eslint/no-require-imports
-      webSocketFactory: () => new (require('sockjs-client'))(wsUrl),
+      brokerURL: wsUrl,
       connectHeaders: {
         userId: userId,
       },
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
       reconnectDelay: 2000,
       onConnect: () => {
         stompClient.subscribe(topic, (message: IMessage) => {
