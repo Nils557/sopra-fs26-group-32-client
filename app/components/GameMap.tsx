10b6107cb8ea714b3051691dba5fa72ee31801e5
Nils <nilsojustdoesit@gmail.com>
2026-04-21 16:27:05 +0200

    implemented map pointer on mapclick close #22

--- app/components/GameMap.tsx
+++ app/components/GameMap.tsx
@@ -1,10 +1,25 @@
 "use client";
 
   import { useState } from "react";
-  import { MapContainer, TileLayer } from "react-leaflet";
+  import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+  import L from "leaflet";
+
+  delete (L.Icon.Default.prototype as any)._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  });
 
   export default function GameMap() {
     const [expanded, setExpanded] = useState(false);
+
+    const [pin, setPin] = useState<{ lat: number; lng: number } | null>(null);
+
+    function ClickHandler() {
+      useMapEvents({ click: (e) => setPin({ lat: e.latlng.lat, lng: e.latlng.lng }) });
+      return null;
+    }
 
     return (
       <div
@@ -12,7 +27,7 @@
         onMouseLeave={() => setExpanded(false)}
         style={{
           position: "absolute",
-          bottom: 0,
+          bottom: 10,
           left: 0,
           width: expanded ? "400px" : "200px",
           height: expanded ? "300px" : "150px",
@@ -28,6 +43,8 @@
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           />
+          <ClickHandler />
+          {pin && <Marker position={[pin.lat, pin.lng]} />}
         </MapContainer>
       </div>
     );
